448e42782311e1d57313a79c7c8e918278bf57ad
Keerthana MS <keerthanamswamy@gmail.com>
2023-11-17 21:37:07 +0530

    added certificate section

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+import CertificateCard from "./CertificateCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
@@ -9,21 +9,21 @@ import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
-function Projects() {
+function Certificates() {
   return (
-    <Container fluid className="project-section">
+    <Container fluid className="certificate-section">
       <Particle />
       <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+        <h1 className="certificate-heading">
+          My Recent <strong className="purple">Certificates </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few Certificates and Badges I have received.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify} //chnage the image here 
+              imgPath={chatify} //change the image here 
               isBlog={false}
               title="Personal Portfolio"
               description="This is the project that you are currently looking at, I have used React, Node.JS, CSS3 and VSCode.
@@ -96,4 +96,4 @@ function Projects() {
   );
 }
 
-export default Projects;
+export default Certificates;
